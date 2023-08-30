68e52f8ae8e89a9bf13411863c4002b3d5fe741b
lukinhaxx <126528237+lukinhaxx@users.noreply.github.com>
2023-08-30 16:24:24 -0300

    Initial commit

---
+++ app/src/main/java/com/example/appdefarmcia/TelaProdutos.kt
@@ -0,0 +1,11 @@
+package com.example.appdefarmcia
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class TelaProdutos : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tela_produtos)
+
+    }
+}
